3f8c2344f302358aa4164a21a4a64f2d68f34c40
Dawid <95036576+Shongini@users.noreply.github.com>
2022-07-06 13:38:29 +0200

    final version 3.0

--- Projekt 2 - Obiektowe.py
+++ Projekt 2 - Obiektowe.py
@@ -1,15 +1,14 @@
 class Zegarek:
-    Marka = ""
-    Rokprodukcji = ""
-    Kolor = ""
 
-    def __init__(self,marka,rokprodukcji,kolor):
+    def __init__(self,marka,model, rokprodukcji,kolor,rozmiar):
         self.marka = marka
+        self.model = model
         self.rokprodukcji = rokprodukcji
         self.kolor = kolor
+        self.rozmiar = rozmiar
 
     def producent(self):
-        print("To Zegarek marki", self.marka)
+        print("To Zegarek marki", self.marka, "oznaczenie modelowe", self.model)
 
     def wyprodukowanie(self):
         if 2022 - int(self.rokprodukcji) >= 2022-2004:
@@ -21,14 +20,26 @@ class Zegarek:
 
     def barwa(self):
         if self.kolor == "Czarny":
-            print("Jest dzisiaj w kiepskim humorze.")
+            print("Jego kolor sprawia, że jest dzisiaj w kiepskim humorze.")
         else:
-            print("Uwielbia być przez Ciebie noszony.")
+            print("Jego kolor poprawia humor każdej osobie, która go zobaczy.")
+    def wielkosc(self):
+        if int(self.rozmiar) > 32:
+            print("Ze względu na średnicę twojego kumpla na nadgarstku, zgaduję że jesteś mężczyzną.")
+        else:
+            print("Sądzę że jesteś kobietą, ponieważ tylko one noszą takie drobniutkie zegarki :)")
 
-Przyjaciel = Zegarek(input("Podaj Marke Zegarka: "), int(input("Podaj Rok Produkcji: ")), input("Podaj Kolor: "))
+Marka1 = input("Podaj Marke Zegarka: ")
+Model1 = input("Podaj jego oznaczenie modelowe: ")
+Rokprodukcji1 = int(input("Podaj Rok Produkcji: "))
+Kolor1 = input("Podaj Kolor: ")
+Rozmiar1 = input("Podaj Średnicę Zegarka w cm: ")
 
-print ("Obiekt - Zegarek", Przyjaciel.marka,",", Przyjaciel.rokprodukcji,"Rok Produkcji",",","Kolor:", Przyjaciel.kolor, "\n")
+Przyjaciel = Zegarek(Marka1,Model1,Rokprodukcji1,Kolor1,Rozmiar1)
+
+print ("\n","Obiekt - ", Przyjaciel.marka,"-","Model: ",Przyjaciel.model,",", "Rok Produkcji",Przyjaciel.rokprodukcji,",","kolor zegarka: ", Przyjaciel.kolor,", rozmiar", Przyjaciel.rozmiar, "centymetrów.","\n")
 
 Przyjaciel.producent()
 Przyjaciel.wyprodukowanie()
 Przyjaciel.barwa()
+Przyjaciel.wielkosc()
